ede4b7447882b1c9baeee591513e8de547231875
Denison Kolling <denisonkolling@gmail.com>
2023-12-27 11:02:24 -0300

    feat: update customer service methods

--- src/main/java/com/example/customerapi/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/example/customerapi/service/impl/CustomerServiceImpl.java
@@ -1,55 +1,69 @@
 package com.example.customerapi.service.impl;
 
 import com.example.customerapi.model.Customer;
+import com.example.customerapi.repository.CustomerRepository;
 import com.example.customerapi.service.CustomerService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
 
     @Override
     public List<Customer> selectAllCustomers() {
-        return null;
+        Page<Customer> page = customerRepository.findAll(Pageable.ofSize(1000));
+        return page.getContent();
     }
 
     @Override
-    public Optional<Customer> selectCustomerById(Integer customerId) {
-        return Optional.empty();
+    public Optional<Customer> selectCustomerById(Integer id) {
+        return customerRepository.findById(id);
     }
 
     @Override
     public void insertCustomer(Customer customer) {
-
+        customerRepository.save(customer);
     }
 
     @Override
     public boolean existsCustomerWithEmail(String email) {
-        return false;
+        return customerRepository.existsCustomerByEmail(email);
     }
 
     @Override
-    public boolean existsCustomerById(Integer customerId) {
-        return false;
+    public boolean existsCustomerById(Integer id) {
+        return customerRepository.existsCustomerById(id);
     }
 
     @Override
     public void deleteCustomerById(Integer customerId) {
-
+        customerRepository.deleteById(customerId);
     }
 
     @Override
     public void updateCustomer(Customer update) {
-
+        customerRepository.save(update);
     }
 
     @Override
     public Optional<Customer> selectUserByEmail(String email) {
-        return Optional.empty();
+        return customerRepository.findCustomerByEmail(email);
     }
 
     @Override
-    public void updateCustomerProfileImageId(String profileImageId, Integer customerId) {
-
+    public void updateCustomerProfileImageId(String profileImageId,
+                                             Integer customerId) {
+        customerRepository.updateProfileImageId(profileImageId, customerId);
     }
 }
