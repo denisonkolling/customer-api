a54aae62cb368ae56b74f291ebcec95dbeabd52d
Denison Kolling <denisonkolling@gmail.com>
2024-01-02 09:25:07 -0300

    add s3 service

--- src/main/java/com/example/customerapi/s3/S3Service.java
+++ src/main/java/com/example/customerapi/s3/S3Service.java
@@ -1,2 +1,26 @@
-package com.example.customerapi.s3;public class S3Service {
+package com.example.customerapi.s3;
+
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@Service
+public class S3Service {
+
+    private final S3Client s3Client;
+
+    public S3Service(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    public void putObject(String bucketName, String key, byte[] file) {
+        PutObjectRequest objectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build();
+        s3Client.putObject(objectRequest, RequestBody.fromBytes(file));
+    }
+
+
 }
